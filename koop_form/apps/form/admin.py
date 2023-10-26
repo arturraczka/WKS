11ded9599321e9b006a574d624c7a43845b3ed72
Artur Rączka <art.raczka@gmail.com>
2023-10-26 15:50:29 +0200

    feat: weight_schemes available in product admin page

--- koop_form/apps/form/admin.py
+++ koop_form/apps/form/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from apps.form.models import Producer, WeightScheme, Status, Product, Order, OrderItem
+from apps.form.models import Producer, WeightScheme, Status, Product, Order, OrderItem, product_weight_schemes
+
+
+class ProductWeightSchemeInLine(admin.TabularInline):
+    model = product_weight_schemes
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = (ProductWeightSchemeInLine,)
+
 
 admin.site.register(Producer)
 admin.site.register(WeightScheme)
 admin.site.register(Status)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
