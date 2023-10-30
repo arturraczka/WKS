1ce29234bd5bc318c737f614eee90e71dafc3623
Artur Rączka <art.raczka@gmail.com>
2023-10-30 11:45:16 +0100

    refact: OrderItem model save() method

--- koop_form/apps/form/models.py
+++ koop_form/apps/form/models.py
@@ -72,15 +72,12 @@ class Product(models.Model):
     )
     quantity_in_stock = models.DecimalField(
         max_digits=9, decimal_places=3, null=True, blank=True
-    )  # if not null,
-    # order_max_quantity = quantity_in_stock???????????????
+    )
     price = models.DecimalField(max_digits=7, decimal_places=2)
     order_deadline = models.DateTimeField(null=True, blank=True)
     quantity_delivered_this_week = models.DecimalField(
         max_digits=9, decimal_places=3, default=0, null=True, blank=True
-    )  # do I
-    # need this?
-    # weight_schemes = models.ManyToManyField(WeightScheme, related_name="products")
+    )
     weight_schemes = models.ManyToManyField(WeightScheme, related_name="products", through="product_weight_schemes")
     is_visible = models.BooleanField(default=True)
     statuses = models.ManyToManyField(Status, related_name="products", blank=True)
@@ -94,7 +91,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    def set_product_visibility(self):
+    def set_product_visibility(self):  # not tested
         if self.is_visible:
             self.is_visible = False
         else:
@@ -139,7 +136,7 @@ class Order(models.Model):
         return f"{self.user}: " f"Order: {self.pk}"
 
     def save(self, *args, **kwargs):
-        self.order_number = calculate_order_number(Order)  # do zrobienia test
+        self.order_number = calculate_order_number(Order)  # not tested
         super(Order, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
@@ -172,18 +169,13 @@ class OrderItem(models.Model):
         return f"{self.order}: " f"{self.product}"
 
     def save(self, *args, **kwargs):
-        if not self.pk and self.quantity == 0:
-            raise ValidationError("Ilość zamawianego produktu nie może być równa 0.")
-        # potencjalnie już do usunięcia
-
         if self.quantity == 0:
-            self.delete()
+            try:
+                self.delete()
+            except ValueError:
+                return None
         else:
             super(OrderItem, self).save(*args, **kwargs)
-
-    # def get_absolute_url(self):
-    #     return reverse('producer-product-list', kwargs={'producer': self.product.producer.name})
-    # czy to ma sens? nie ma, bo nie do tego to służy
 
 
 # CELERY TASKS:
