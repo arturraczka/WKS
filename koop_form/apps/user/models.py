33579e32bed69965df4b446913bacf6064f46991
Artur Rączka <art.raczka@gmail.com>
2023-11-09 11:28:11 +0100

    fix: changed field name of UserProfile model from budget to fund. Fixed choices and default value for this field by using Decimal() on a string

--- koop_form/apps/user/models.py
+++ koop_form/apps/user/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from decimal import Decimal
 
 ModelUser = get_user_model()
 
 
 class UserProfile(models.Model):
-    BUDGET_CHOICES = [
-        (1.1, 1.1),
-        (1.3, 1.3),
+    FUND_CHOICES = [
+        (Decimal('1.1'), 1.1),
+        (Decimal('1.3'), 1.3),
     ]
     user = models.OneToOneField(ModelUser, on_delete=models.CASCADE)
-    budget = models.DecimalField(max_digits=3, decimal_places=1, choices=BUDGET_CHOICES, blank=True, default=1.3)
+    fund = models.DecimalField(max_digits=3, decimal_places=1, choices=FUND_CHOICES, blank=True, default=Decimal('1.3'))
     phone_number = models.PositiveIntegerField(blank=True, null=True)
