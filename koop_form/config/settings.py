0521e3ea047402f3ff954b156afefc7b24ed74cb
Artur Rączka <art.raczka@gmail.com>
2023-11-13 15:28:20 +0100

    feat: added basic email sending settings

--- koop_form/config/settings.py
+++ koop_form/config/settings.py
@@ -203,7 +203,16 @@ LOGGING = {
 }
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_TIMEOUT = 30
+
 
 # na ten moment walidacje idą jako INFO i są nierozróżnialne od prawidłowych requestów hmmmm
