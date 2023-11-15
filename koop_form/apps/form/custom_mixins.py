549e34d9a7f90158a35cfb0a21404ff3da6cca74
Artur Rączka <art.raczka@gmail.com>
2023-11-15 19:56:51 +0100

    fix: fixed OrderExistsTestMixin, now instead of returning 403, it redirects a user to OrderCreateView

--- koop_form/apps/form/custom_mixins.py
+++ koop_form/apps/form/custom_mixins.py
@@ -1,6 +1,27 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, AccessMixin
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
+from urllib.parse import urlparse
+
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.views import redirect_to_login
+from django.core.exceptions import ImproperlyConfigured, PermissionDenied
+from django.shortcuts import resolve_url
+from django.urls import reverse
 
 
 class OrderExistsTestMixin(UserPassesTestMixin):
-    login_url = "/formularz/zamowienie/nowe/"
-    permission_denied_message = "Najpierw utwórz nowe zamówienie na ten tydzień."
+    def get_permission_denied_message(self):
+        return "Najpierw utwórz nowe zamówienie na ten tydzień."
+
+    def get_login_url(self):
+        """
+        Override this method to return the specific URL where you want to
+        redirect the user in case the test fails.
+        """
+        # You can return a specific URL here.
+        return reverse('order-create')
+
+    def handle_no_permission(self):
+        return redirect(self.get_login_url())
