cb842b04d6fbc6f7c61f1fa3461ddf85a04961e8
Artur Rączka <art.raczka@gmail.com>
2023-11-23 09:11:16 +0100

    removed signal creating user profile - admins will create them manually

--- koop_form/apps/user/signals.py
+++ koop_form/apps/user/signals.py
@@ -1,13 +1,13 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from apps.user.models import UserProfile
-from django.contrib.auth import get_user_model
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from apps.user.models import UserProfile
+# from django.contrib.auth import get_user_model
+#
+# ModelUser = get_user_model()
+#
 
-ModelUser = get_user_model()
-
-
-@receiver(post_save, sender=ModelUser)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        profile = UserProfile(user=instance)
-        profile.save()
+# @receiver(post_save, sender=ModelUser)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         profile = UserProfile(user=instance)
+#         profile.save()
